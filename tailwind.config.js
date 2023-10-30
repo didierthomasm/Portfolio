6c3e08b29e5414eb7235d2f9e26e4ba975a46a58
didierthomasm <didierthomas.m@gmail.com>
2023-10-30 13:18:57 -0600

    Add colors Cardinal, Bright-pink-crayola, Robin-egg-blue, Orange-crayola, and Licorice to the tailwind.config.js file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ export default {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        Cardinal: '#B42741',
+        'Bright-pink-crayola': '#F14D6E',
+        'Robin-egg-blue': '#1FD5D5',
+        'Orange-crayola': '#E17A40',
+        Licorice: '#0F0108'
+      },
+    },
   },
   plugins: [],
 }
