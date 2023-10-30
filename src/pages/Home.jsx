dc63d3a2dece52d23894185f2bb17705c5339d6a
didierthomasm <didierthomas.m@gmail.com>
2023-10-30 13:19:43 -0600

    Update Home.jsx to remove contact button.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ export default function Home() {
       <div className="text-4xl md:text-5xl font-bold">Hi,</div>
       <div className="text-4xl md:text-5xl">I'm Didier</div>
       <div className="text-2xl md:text-3xl text-blue-600">Web Developer</div>
-      <button className="bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 focus:outline-none">Contact</button>
     </div>
   )
 }
