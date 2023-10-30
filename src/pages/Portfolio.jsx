54edbd90ad263652f203d5fb61a23434d13265f3
didierthomasm <didierthomas.m@gmail.com>
2023-10-30 13:20:11 -0600

    Refactor Portfolio component to render a grid of project images.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,17 @@
+const imgs = [
+  {src: 'pto1', alt: 'First project portfolio'},
+]
 export default function Portfolio() {
   return (
-    <></>
+    <main className='grid grid-cols-3 gap-4'>
+      {imgs.map((im))}
+{/*      {imgs.map((img) => {
+      <div>
+
+          <img src={img.src} alt={img.alt} className='h-36'/>
+
+      </div>
+      })}*/}
+    </main>
   )
 }
