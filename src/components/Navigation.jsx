4ec4c2b8c0c440867dd83253fca9cf6886b6827c
didierthomasm <didierthomas.m@gmail.com>
2023-10-30 18:40:29 -0600

    Refactor Portfolio.jsx to import images from portfolioImages.js and use them in the component.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 
 const links = [
   {path: '/', label: 'Home'},
-  {path: '/AboutMe', label: 'About'},
+  {path: '/AboutMe', label: 'About Me'},
   {path: '/Portfolio', label: 'Portfolio'},
   {path: '/Resume', label: 'Resume'},
   {path: '/Contact', label: 'Contact'}
