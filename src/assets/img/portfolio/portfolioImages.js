932fca50ef4964b9f0a0a85b5c902b01b8bd9521
didierthomasm <didierthomas.m@gmail.com>
2023-10-30 18:39:58 -0600

    Refactor Error.jsx and error.js to import images from a centralized file.

---
+++ src/assets/img/portfolio/portfolioImages.js
@@ -0,0 +1,6 @@
+export { default as pto1 } from './StarChild-portfolio1.png';
+export { default as pto2 } from './BlogerVerse-portfolio2.png';
+export { default as pto3 } from './TextEditor-portfolio3.jpg';
+export { default as pto4 } from './PasswordGenerator-portfolio4.jpg';
+export { default as pto5 } from './DietWizard-portfolio5.jpg';
+export { default as pto6 } from './WeatherChanel-portfolio6.jpg';
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,17 +1,21 @@
-const imgs = [
-  {src: 'pto1', alt: 'First project portfolio'},
+import * as portfolioImages from '../assets/img/portfolio/portfolioImages.js';
+
+const images = [
+  {src: portfolioImages.pto1, alt: 'Universe Map', href: 'https://didierthomasm.github.io/Project-One/'},
+  {src: portfolioImages.pto2, alt: 'Blog', href: 'https://glacial-beach-50205-ef9587d9a5cc.herokuapp.com/'},
+  {src: portfolioImages.pto3, alt: 'Text Editor', href: 'https://still-coast-34648-3df93645c337.herokuapp.com/'},
+  {src: portfolioImages.pto4, alt: 'Password Generator', href: 'https://didierthomasm.github.io/js-awesome-password-generator'},
+  {src: portfolioImages.pto5, alt: 'Diet Generator', href: 'https://diet-wizard-930125214349.herokuapp.com/'},
+  {src: portfolioImages.pto6, alt: 'Weather Page', href: 'https://didierthomasm.github.io/Weather-Chanel/'},
 ]
 export default function Portfolio() {
   return (
-    <main className='grid grid-cols-3 gap-4'>
-      {imgs.map((im))}
-{/*      {imgs.map((img) => {
-      <div>
-
-          <img src={img.src} alt={img.alt} className='h-36'/>
-
-      </div>
-      })}*/}
+    <main className='grid md:mt-16 md:grid-cols-3 gap-4 mt-32 grid-cols-1'>
+      {images.map((image) => (
+        <div key={image.src} className='flex justify-center shadow-2xl w-38 hover:drop-shadow-2xl'>
+          <a href={image.href} target='_blank' rel='noopener noreferrer'><img src={image.src} alt={image.alt} className='h-36'/></a>
+        </div>
+      ))}
     </main>
   )
 }
