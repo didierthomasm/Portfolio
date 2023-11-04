0cfa0f87136e81d2101585ffa53fa833a44e46a3
didierthomasm <didierthomas.m@gmail.com>
2023-11-04 03:40:16 -0600

    Update Portfolio page styling to include additional padding.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,7 @@
 import Project from '../components/Project.jsx'
 export default function Portfolio() {
   return (
-    <main className='grid md:mt-16 md:grid-cols-3 gap-4 mt-32 grid-cols-1'>
+    <main className='grid md:mt-16 md:grid-cols-3 gap-4 mt-32 grid-cols-1 p-4 md:p-8'>
       <Project />
     </main>
   )
