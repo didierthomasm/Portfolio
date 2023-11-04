ac50d4fe7c8250a4bc1f4cadb1aa18c404326b47
didierthomasm <didierthomas.m@gmail.com>
2023-11-04 03:40:32 -0600

    Refactor Home component to include a main tag for better structure.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center space-y-4 md:space-y-8">
-      <div className="text-4xl md:text-5xl font-bold">Hi,</div>
-      <div className="text-4xl md:text-5xl">I'm Didier</div>
-      <div className="text-2xl md:text-3xl text-blue-600">Web Developer</div>
-    </div>
+    <main className="p-4 md:p-8">
+      <div className="flex flex-col items-center space-y-4 md:space-y-8">
+        <div className="text-4xl md:text-5xl font-bold">Hi,</div>
+        <div className="text-4xl md:text-5xl">I'm Didier</div>
+        <div className="text-2xl md:text-3xl text-blue-600">Web Developer</div>
+      </div>
+    </main>
   )
 }
